8a35687e71da9295e1bd60882f7e80eb9e034738
liwanyi <you@example.co3357702725@qq.com>
2023-12-13 23:42:39 +0800

    列表

--- src/Redis.php
+++ src/Redis.php
@@ -100,4 +100,78 @@ class Redis
         return $this->redis->rPop($key, $count);
     }
 
+
+    /**
+     * 返回列表长度
+     * @param $list_name
+     * @return bool|int|\Redis
+     * @throws \RedisException
+     */
+    public function lLenList($list_name)
+    {
+        return $this->redis->lLen($list_name);
+    }
+
+    /**
+     * 返回指定范围内的长度( 默认返回全部数据)
+     * @param $list_name
+     * @param $start
+     * @param $end
+     * @return array|\Redis
+     * @throws \RedisException
+     */
+    public function lRangeValue($list_name, $start = 0, $end = -1)
+    {
+        return $this->redis->lRange($list_name, $start, $end);
+    }
+
+    /**
+     * 删除指定数量 value 的值
+     *  LREM list -2 “hello” 会从列表key中删除最后两个出现的 “hello”
+     * @param $key
+     * @param $value
+     * @param $count 可以大于0 可以小于0 可以等于0 默认是0
+     * @return bool|int|\Redis
+     * @throws \RedisException
+     */
+    public function lRemValue($key, $value, $count =0)
+    {
+        if (!$key || empty($value)) {
+            return '';
+        }
+        return $this->redis->lrem($key, $value, $count );
+    }
+
+
+    /**
+     * 用于设置列表 key 中 index 位置的元素值为 element
+     * 默认设置的是第一个
+     * @param $key
+     * @param $value
+     * @param $index
+     * @return bool|\Redis|string
+     * @throws \RedisException
+     */
+    public function lSetValue($key, $value, $index = 0){
+        if (!$key || empty($value)) {
+            return '';
+        }
+        return $this->redis->lSet($key, $index, $value);
+    }
+
+    /**
+     * 保证列表的长度
+     * @param $list_name
+     * @param $start
+     * @param $end
+     * @return array|false|\Redis
+     * @throws \RedisException
+     */
+    public function lLTrim($list_name,$start,$end){
+        if(empty($list_name) || empty($start) || empty($end)){
+            return '';
+        }
+        return $this->redis->lTrim($list_name,$start,$end);
+    }
+
 }
