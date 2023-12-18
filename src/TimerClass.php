800551728c582e2b67dee5bd1d73228f6ddf704f
liwanyi <you@example.co3357702725@qq.com>
2023-12-18 23:05:55 +0800

    asdg

--- src/TimerClass.php
+++ src/TimerClass.php
@@ -1,23 +1,10 @@
 <?php
 
 namespace Liwanyi\Utils2;
-use Workerman\Timer;
-use Workerman\Worker;
 class TimerClass
 {
     public function test(){
-        $task = new Worker();
-        // 开启多少个进程运行定时任务，注意业务是否在多进程有并发问题
-        $task->count = 1;
-        $task->onWorkerStart = function (Worker $task)
-        {
-            // 每2.5秒执行一次
-            $time_interval = 2.5;
-            Timer::add($time_interval, function () {
-                echo "task run\n";
-            });
-        };
-        Worker::runAll();
+       echo 3;
     }
 
 
