25d455f88279b2816f4f3c0fecbfa2c36f6e037a
李万义 <lwi0801@163.com>
2024-09-11 20:39:54 +0800

    a

--- src/test/Test.php
+++ src/test/Test.php
@@ -5,10 +5,6 @@ namespace Liwanyi\Utils2\test;
 use Liwanyi\Utils2;
 use Liwanyi\Utils2\Redis;
 class Test{
-    public function test(){
-        $redis = new Redis();
-       return $redis->hset('name','liwanyi',1);
-    }
 
 }
 $result = (new Test())->test();
