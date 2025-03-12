30151f9d88866bad444c2d074e43b2345d61a647
李万义 <3357702725@qq.com>
2025-03-13 00:12:47 +0800

    去除php8中的方法 遵循7

--- src/test/Test.php
+++ src/test/Test.php
@@ -1,21 +1,22 @@
 <?php
-
 namespace Liwanyi\Utils2\test;
+// 一定要进行加载引入
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Liwanyi\Utils2;
-use Liwanyi\Utils2\Redis;
+use Liwanyi\Utils2\ArrayHelper;
+use Liwanyi\Utils2\StringHelper;
+
 class Test{
+    
+    public function test(){
+        $data = [
+            '1'=>'榨干啊',
+            '2'=>'里斯'
+        ];
+        return ArrayHelper::getFirstValue($data);
 
+//        return StringHelper::explodeString('123456');
+    }
 }
 $result = (new Test())->test();
 var_dump($result);
-
-
-
-
-
-
-
-
-
-
